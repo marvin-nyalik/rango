9aebf875d28032e1e34f4ec143e9d186b5f3a034
MARVIN <o.silverygold@gmail.com>
2024-02-23 21:29:36 +0300

    Define app layout

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,12 @@
+import AddPostForm from "../../components/addPostForm";
+
+export default async function Posts() {
+    
+    return (
+      <>
+        <AddPostForm />
+        <hr />
+        <h2 className="mx-auto">Posts</h2>
+      </>
+    );
+  }
