70182fc17bd0c292bcefe73f0142dbe02b67a9f2
MARVIN <o.silverygold@gmail.com>
2024-02-24 15:16:28 +0300

    Add new post form

---
+++ src/app/posts/new/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import AddPostForm from "@/components/addPostForm";
+
+const NewPost: React.FC = () => {
+  
+  return (
+    <>
+    <AddPostForm/>
+    </>
+  )
+}
+
+export default NewPost;
