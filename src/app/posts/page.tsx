680fc2075a622a39e4e20c666868cad15767675a
MARVIN <o.silverygold@gmail.com>
2024-02-24 15:18:37 +0300

    Create Post Form component

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,12 +1,24 @@
 import AddPostForm from "../../components/addPostForm";
+import { PrismaClient } from "@prisma/client";
 
 export default async function Posts() {
-    
-    return (
-      <>
-        <AddPostForm />
-        <hr />
-        <h2 className="mx-auto">Posts</h2>
-      </>
-    );
-  }
+  const prisma = new PrismaClient();
+  const posts = await prisma.post.findMany();
+
+  return (
+    <>
+      <AddPostForm />
+      <hr />
+      <h2 className="font-bold mx-auto text-xl flex justify-center items-center">Posts</h2>
+      <div className="mx-auto max-w-[90%] flex flex-col space-y-4">
+        {posts.map((post, index) => (
+          <p key={index}
+            className="leading-loose text-lg font-light"
+          >
+            {post.title}
+          </p>
+        ))}
+      </div>
+    </>
+  );
+}
--- src/components/addPostForm.tsx
+++ src/components/addPostForm.tsx
@@ -1,32 +1,38 @@
 'use client';
 import addPost from "@/app/actions/addPostAction";
+import Submit from "./submit";
 
 export default async function AddPostForm() {
   return (
     <>
-    <div className="w-full max-w-[80%] flex flex-col mx-auto">
+      <div className="w-full max-w-[80%] flex flex-col mx-auto">
           <h2 className="font-semibold text-md my-2 mx-auto">Create Post Using Server Actions</h2>
-
             <form action={addPost}>
-              <div className="flex flex-col h-[70vh] border border-gray-500">
+              <div className="flex flex-col h-[70vh]">
                 <div className="flex justify-evenly items-center space-x-4 space-y-4 mb-3">
-                  <input className="p-1 w-full mx-auto" id="title" name="text" type="text"
-                    placeholder="title"
-                    required
+                  <input className="p-1 w-full mx-auto border-2 border-gray-900 rounded-xl mb-2" 
+                   id="title" 
+                   name="title" 
+                   type="text"
+                   placeholder="title"
+                   required
                   />
                 </div>
-                <div className="flex justify-evenly items-center space-x-4 space-y-4 mb-3">
-                  <input className="p-1 w-full mx-auto" id="body" name="text" type="text"
+                <div className="flex justify-evenly h-[30vh] items-center space-x-4 space-y-4 my-3">
+                  <input className="p-1 w-full h-[100%] border-2 border-gray-900 mx-auto" 
+                    id="body" 
+                    name="body" 
+                    type="text"
                     placeholder="body"
                     required
                   />
                 </div>
               </div>
               <div>
-                <button type="submit" className="border border-gray-200 rounded-xl bg-emerald-500 p-1 my-2 text-white">Create Post</button>
+                <Submit />
               </div>
             </form>
-          </div>
+      </div>
     </>
   )
 }
