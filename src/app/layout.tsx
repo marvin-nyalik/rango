40696bfc42efb352607bd3a72dc7ae8d1e1254fa
MARVIN <o.silverygold@gmail.com>
2024-02-25 13:34:00 +0300

    Include Toaster, Navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Toaster position="top-right" />
+      </body>
       <Footer />
     </html>
   );
