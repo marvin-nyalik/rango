d14b75ab4804f047c1a61a2cdafd5109f681e47a
MARVIN <o.silverygold@gmail.com>
2024-02-25 13:37:00 +0300

    Use toast component, navigate with useRouter hook

--- src/components/addPostForm.tsx
+++ src/components/addPostForm.tsx
@@ -1,13 +1,27 @@
 'use client';
-import addPost from "@/app/actions/addPostAction";
-import Submit from "./submit";
+import { addPost } from "@/app/actions/addPostAction";
+import { useRouter } from "next/navigation";
+import { toast } from "react-hot-toast";
 
-export default async function AddPostForm() {
+export default function AddPostForm() {
+
+  const { replace } = useRouter();
+  async function clientAction(formData: FormData) {
+    const result = await addPost(formData);
+    if (result?.error) {
+      toast.error(result.error);
+    }
+    else {
+      replace(`/posts`);
+      toast.success('Post created');
+    }
+  }
+
   return (
     <>
       <div className="w-full max-w-[80%] flex flex-col mx-auto">
           <h2 className="font-semibold text-md my-2 mx-auto">Create Post Using Server Actions</h2>
-            <form action={addPost}>
+            <form action={clientAction}>
               <div className="flex flex-col h-[70vh]">
                 <div className="flex justify-evenly items-center space-x-4 space-y-4 mb-3">
                   <input className="p-1 w-full mx-auto border-2 border-gray-900 rounded-xl mb-2" 
@@ -29,7 +43,11 @@ export default async function AddPostForm() {
                 </div>
               </div>
               <div>
-                <Submit />
+              <button type="submit"
+                className="border border-gray-200 disabled:bg-zinc-500 transition rounded-xl bg-emerald-500 p-1 my-2 text-white"
+              >
+                Create Post
+              </button>
               </div>
             </form>
       </div>
