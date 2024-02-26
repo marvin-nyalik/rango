782ce315aedf4037e81cd4da89a4054718af4c9c
MARVIN <o.silverygold@gmail.com>
2024-02-26 20:29:20 +0300

    Include delete action in posts page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import Link from "next/link";
+import DeleteForm from "@/components/deletePostForm";
 
 export default async function Posts() {
   const prisma = new PrismaClient();
@@ -7,13 +9,27 @@ export default async function Posts() {
   return (
     <>
       <h2 className="font-bold mx-auto text-xl flex justify-center items-center">Posts</h2>
-      <div className="mx-auto max-w-[90%] flex flex-col space-y-4">
+      <div className="mx-auto max-w-[90%] flex flex-col space-y-4 my-3 shadow-2xl">
         {posts.map((post, index) => (
-          <p key={index}
+          <div className="flex justify-between w-full mx-auto" key={post.id}>
+          <Link
+          href={{
+            pathname:`/posts/${encodeURIComponent(post.title)}`,
+            query:{
+              id: post.id,
+              title: post.title,
+              body: post.body
+            }
+          }}
+          >
+          <div key={index}
             className="leading-loose text-lg font-light"
           >
-            {post.title}
-          </p>
+            <p> {post.title} </p>
+          </div>
+          </Link>
+          <DeleteForm id={post.id}/>
+          </div>
         ))}
       </div>
     </>
